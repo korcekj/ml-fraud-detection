8a7ea4c678d8e4d51123c7dcb830ec60c6f70b71
Ján Korček <korcekdevelopment@gmail.com>
2022-03-17 23:02:45 +0100

    refactor: make model purely abstract

--- src/model.py
+++ src/model.py
@@ -8,7 +8,6 @@ class Model(ABC):
     """
     An abstract class used to define the schema of each module/model
     """
-    visuals = {}
 
     @classmethod
     @abstractmethod
@@ -34,31 +33,3 @@ class Model(ABC):
     @abstractmethod
     def evaluate(self, test_data: Data):
         pass
-
-    def visualize(self, key: DataType, dir_path: str):
-        """
-        Show or export Visualization object
-        :param key: type of visualization
-        :param dir_path: path to directory
-        """
-        if key not in self.visuals:
-            raise Exception('Key does not exists')
-
-        if dir_path and not IO.is_dir(dir_path):
-            raise Exception('Folder does not exist')
-
-        for index, visual in enumerate(self.visuals[key], 1):
-            if dir_path is None:
-                visual.show()
-            else:
-                visual.export(f'{dir_path}/{key}.{index}')
-
-    def _visualize(self, key: DataType, vis: Visualization):
-        """
-        Add Visualization object
-        :param key: type of visualization
-        :param vis: Visualization object
-        """
-        if key not in self.visuals:
-            self.visuals[key] = []
-        self.visuals[key].append(vis)
